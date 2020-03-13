3d62cdf9f2d2481b55fa0e778bdf3ea350df62f2
Fabio Cicerchia <info@fabiocicerchia.it>
2020-03-13 19:25:21 +0100

    switch between region and province

--- public/script.js
+++ public/script.js
@@ -1,3 +1,4 @@
+// UTILITIES
 function heatMapColorforValue(value) {
     var h = (1.0 - value) * 240
     return "hsl(" + h + ", 100%, 50%)";
@@ -6,30 +7,14 @@ function heatMapColorforValue(value) {
 function fetchJSONFile(path, callback) {
     var httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = function() {
-        if (httpRequest.readyState === 4) {
-            if (httpRequest.status === 200) {
-                var data = JSON.parse(httpRequest.responseText);
-                if (callback) callback(data);
-            }
+        if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+            var data = JSON.parse(httpRequest.responseText);
+            if (callback) callback(data);
         }
     };
     httpRequest.open('GET', path);
     httpRequest.send();
 }
-
-var sliderControl = null;
-var map = L.map('map').setView([41.8719, 12.5674], 6);
-
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZmFiaW9jaWNlcmNoaWEiLCJhIjoiY2s3b2phNXhxMDlzbTNncDkzY3pkb2YxYSJ9.37ZdJEamTJMHvbticJ25CQ', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox/light-v9'
-}).addTo(map);
-
-document.getElementById('search-form').onsubmit = function(e) {
-    document.getElementById('value-form').value = normalisePlace(document.getElementById('value-form').value);
-};
 
 function normalisePlace(place) {
     var lowerPlace = place.toLowerCase();
@@ -64,22 +49,26 @@ function normalisePlace(place) {
     return lowerPlace;
 }
 
-history = {};
-geoLayer = undefined;
-
-function paintMap(dataProvince, lastUpdate) {
+function paintMap(type, data, lastUpdate) {
     document.getElementById('lastUpdate').innerHTML = lastUpdate;
-    var max = Math.max.apply(Math, Object.values(dataProvince));
+    var max = Math.max.apply(Math, Object.values(data));
 
-    fetchJSONFile('/limits_IT_provinces.geojson', function(provinces) {
+    var endpoint = type === 'region'
+                   ? '/limits_IT_regions.geojson'
+                   : '/limits_IT_provinces.geojson';
+
+    fetchJSONFile(endpoint, function(items) {
         if (geoLayer !== undefined) {
             geoLayer.remove();
         }
-        geoLayer = L.geoJSON(provinces, {
+
+        geoLayer = L.geoJSON(items, {
            weight: 1,
            style: function(feature) {
-               var place = normalisePlace(feature.properties.prov_name);
-               var cases = dataProvince[place];
+               var place = type === 'region'
+                           ? normalisePlace(feature.properties.reg_name)
+                           : normalisePlace(feature.properties.prov_name);
+               var cases = data[place];
                if (cases > 0) {
                    return {
                        color: heatMapColorforValue(Math.min(cases / max + 0.75, 1))
@@ -87,8 +76,10 @@ function paintMap(dataProvince, lastUpdate) {
                }
            },
            onEachFeature: function(feature, layer) {
-               var place = normalisePlace(feature.properties.prov_name);
-               var cases = dataProvince[place];
+               var place = type === 'region'
+                           ? normalisePlace(feature.properties.reg_name)
+                           : normalisePlace(feature.properties.prov_name);
+               var cases = data[place];
                var popupContent = '<strong>' + place + '</strong><br>';
                popupContent += 'Cases: ' + cases + '<br>';
                popupContent += '<small>Updated on ' + lastUpdate + '<small>';
@@ -97,12 +88,34 @@ function paintMap(dataProvince, lastUpdate) {
         })
         .addTo(map)
         .on('click', function(e) {
-            document.getElementById('type-form').value = 'province';
-            document.getElementById('value-form').value = normalisePlace(e.sourceTarget.feature.properties.prov_name);
+            document.getElementById('type-form').value = type;
+            document.getElementById('value-form').value = type === 'region'
+                    ? normalisePlace(e.sourceTarget.feature.properties.reg_name)
+                    : normalisePlace(e.sourceTarget.feature.properties.prov_name);
             document.getElementById('search-form').submit();
         });
     });
 }
+
+// SEARCH FORM
+document.getElementById('search-form').onsubmit = function(e) {
+    document.getElementById('value-form').value = normalisePlace(document.getElementById('value-form').value);
+};
+
+// COVID DATA
+history = {region: {}, province: {}};
+
+// GEOMAP
+var map = L.map('map').setView([41.8719, 12.5674], 6);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZmFiaW9jaWNlcmNoaWEiLCJhIjoiY2s3b2phNXhxMDlzbTNncDkzY3pkb2YxYSJ9.37ZdJEamTJMHvbticJ25CQ', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox/light-v9'
+}).addTo(map);
+
+geoLayer = undefined;
 
 Papa.parse('/dpc-covid19-ita-province.csv', {
     header: true,
@@ -113,46 +126,67 @@ Papa.parse('/dpc-covid19-ita-province.csv', {
         var yesterday = date.getFullYear() + '-' + (date.getMonth() + 1).toString().padStart(2, '0') + '-' + (date.getDate() - 1).toString().padStart(2, '0');
         results.data.forEach(function(item) {
             var parsedDate = item.data.substr(0, 10);
-            if (typeof history[parsedDate] === 'undefined') history[parsedDate] = {};
+            if (typeof history['province'][parsedDate] === 'undefined') history['province'][parsedDate] = {};
 	    province = normalisePlace(item.denominazione_provincia);
-            history[parsedDate][province] = parseInt(item.totale_casi, 10);
+            history['province'][parsedDate][province] = parseInt(item.totale_casi, 10);
         });
-        var dataProvince = history[today] || history[yesterday];
-        var lastUpdate = history[today] ? today : yesterday;
+        var dataProvince = history['province'][today] || history['province'][yesterday];
+        var lastUpdate = history['province'][today] ? today : yesterday;
         var sumCases = Object.values(dataProvince).reduce((a, b) => a + b, 0);
         document.getElementById('totalCases').innerHTML = sumCases.toLocaleString();
 
-        var colours = ['#ff0000', '#ff4e00', '#ff7100', '#ff8d00', '#ffa600', '#ffbe00', '#ffd400', '#ffea00', '#ffff00']
-            .reverse();
+        paintMap('province', dataProvince, lastUpdate);
+    }
+});
 
-        paintMap(dataProvince, lastUpdate);
+Papa.parse('/dpc-covid19-ita-regioni.csv', {
+    header: true,
+    download: true,
+    complete: function(results) {
+        var date = new Date();
+        var today = date.getFullYear() + '-' + (date.getMonth() + 1).toString().padStart(2, '0') + '-' + (date.getDate()).toString().padStart(2, '0');
+        var yesterday = date.getFullYear() + '-' + (date.getMonth() + 1).toString().padStart(2, '0') + '-' + (date.getDate() - 1).toString().padStart(2, '0');
+        results.data.forEach(function(item) {
+            var parsedDate = item.data.substr(0, 10);
+            if (typeof history['region'][parsedDate] === 'undefined') history['region'][parsedDate] = {};
+	    province = normalisePlace(item.denominazione_provincia);
+            history['region'][parsedDate][province] = parseInt(item.totale_casi, 10);
+        });
+        var dataProvince = history['region'][today] || history['region'][yesterday];
+        var lastUpdate = history['region'][today] ? today : yesterday;
+        var sumCases = Object.values(dataProvince).reduce((a, b) => a + b, 0);
+        document.getElementById('totalCases').innerHTML = sumCases.toLocaleString();
     }
 });
 
 currentDate = lastUpdate;
 document.getElementById('dayFirst').onclick = function() {
-    var first = Object.keys(history)[0];
+    var currentType = document.querySelector('input[name="type"]:checked').value;
+    var first = Object.keys(history[currentType])[0];
     lastUpdate = first;
 
-    paintMap(history[first], first);
+    paintMap(currentType, history[currentType][first], first);
 };
 document.getElementById('dayBefore').onclick = function() {
+    var currentType = document.querySelector('input[name="type"]:checked').value;
     var date = new Date(Date.parse(currentDate.innerHTML) - (24 * 60 * 60 * 1000));
     var before = date.getFullYear() + '-' + (date.getMonth() + 1).toString().padStart(2, '0') + '-' + (date.getDate()).toString().padStart(2, '0');
     lastUpdate = before;
 
-    paintMap(history[before], before);
+    paintMap(currentType, history[currentType][before], before);
 };
 document.getElementById('dayAfter').onclick = function() {
+    var currentType = document.querySelector('input[name="type"]:checked').value;
     var date = new Date(Date.parse(currentDate.innerHTML) + (24 * 60 * 60 * 1000));
     var after = date.getFullYear() + '-' + (date.getMonth() + 1).toString().padStart(2, '0') + '-' + (date.getDate()).toString().padStart(2, '0');
     lastUpdate = after;
 
-    paintMap(history[after], after);
+    paintMap(currentType, history[currentType][after], after);
 };
 document.getElementById('dayLast').onclick = function() {
-    var last = Object.keys(history)[Object.keys(history).length - 1];
+    var currentType = document.querySelector('input[name="type"]:checked').value;
+    var last = Object.keys(history[currentType])[Object.keys(history[currentType]).length - 1];
     lastUpdate = last;
 
-    paintMap(history[last], last);
+    paintMap(currentType, history[currentType][last], last);
 };
