c38016eda4bf0b32d3aaa5dcbb352633deb9e436
Fabio Cicerchia <info@fabiocicerchia.it>
2020-03-13 18:45:21 +0100

    fixed #10: Handle province and region in English

--- public/script.js
+++ public/script.js
@@ -27,11 +27,41 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     id: 'mapbox/light-v9'
 }).addTo(map);
 
+document.getElementById('search-form').onsubmit = function(e) {
+    document.getElementById('value-form').value = normalisePlace(document.getElementById('value-form').value);
+};
+
 function normalisePlace(place) {
-    if (place === 'Valle d\'Aosta/Vallée d\'Aoste') return 'Aosta';
-    else if (place === 'Bolzano/Bozen') return 'Bolzano';
-    else if (place === 'Massa-Carrara') return 'Massa Carrara';
-    return place;
+    var lowerPlace = place.toLowerCase();
+
+    // italian
+    if (lowerPlace === 'valle d\'aosta/vallée d\'aoste') return 'aosta';
+    else if (lowerPlace === 'bolzano/bozen') return 'bolzano';
+    else if (lowerPlace === 'massa-carrara') return 'massa carrara';
+    else if (lowerPlace === 'alto adige') return 'p.a. bolzano';
+    else if (lowerPlace === 'trentino') return 'p.a. trento';
+
+    // english
+    if (lowerPlace === 'florence') return 'firenze';
+    else if (lowerPlace === 'genoa') return 'genova';
+    else if (lowerPlace === 'mantua') return 'mantova';
+    else if (lowerPlace === 'milan') return 'milano';
+    else if (lowerPlace === 'naples') return 'napoli';
+    else if (lowerPlace === 'padua') return 'padova';
+    else if (lowerPlace === 'rome') return 'roma';
+    else if (lowerPlace === 'south sardinia') return 'sud sargegna';
+    else if (lowerPlace === 'syracuse') return 'siracusa';
+    else if (lowerPlace === 'turin') return 'torino';
+    else if (lowerPlace === 'venice') return 'venezia';
+
+    if (lowerPlace === 'latium') return 'lazio';
+    else if (lowerPlace === 'apulia') return 'puglia';
+    else if (lowerPlace === 'sardinia') return 'sardegna';
+    else if (lowerPlace === 'sicily') return 'sicilia';
+    else if (lowerPlace === 'south tyrol') return 'p.a. bolzano';
+    else if (lowerPlace === 'south tyrol') return 'p.a. trento';
+
+    return lowerPlace;
 }
 
 history = {};
@@ -46,31 +76,31 @@ function paintMap(dataProvince, lastUpdate) {
             geoLayer.remove();
         }
         geoLayer = L.geoJSON(provinces, {
-                weight: 1,
-                style: function(feature) {
-                    var place = normalisePlace(feature.properties.prov_name);
-                    var cases = dataProvince[place];
-                    if (cases > 0) {
-                        return {
-                            color: heatMapColorforValue(Math.min(cases / max + 0.75, 1))
-                        };
-                    }
-                },
-                onEachFeature: function(feature, layer) {
-                    var place = normalisePlace(feature.properties.prov_name);
-                    var cases = dataProvince[place];
-                    var popupContent = '<strong>' + place + '</strong><br>';
-                    popupContent += 'Cases: ' + cases + '<br>';
-                    popupContent += '<small>Updated on ' + lastUpdate + '<small>';
-                    layer.bindPopup(popupContent);
-                }
-            })
-            .addTo(map)
-            .on('click', function(e) {
-                document.getElementById('type-form').value = 'province';
-                document.getElementById('value-form').value = normalisePlace(e.sourceTarget.feature.properties.prov_name);
-                document.getElementById('search-form').submit();
-            });
+           weight: 1,
+           style: function(feature) {
+               var place = normalisePlace(feature.properties.prov_name);
+               var cases = dataProvince[place];
+               if (cases > 0) {
+                   return {
+                       color: heatMapColorforValue(Math.min(cases / max + 0.75, 1))
+                   };
+               }
+           },
+           onEachFeature: function(feature, layer) {
+               var place = normalisePlace(feature.properties.prov_name);
+               var cases = dataProvince[place];
+               var popupContent = '<strong>' + place + '</strong><br>';
+               popupContent += 'Cases: ' + cases + '<br>';
+               popupContent += '<small>Updated on ' + lastUpdate + '<small>';
+               layer.bindPopup(popupContent);
+           }
+        })
+        .addTo(map)
+        .on('click', function(e) {
+            document.getElementById('type-form').value = 'province';
+            document.getElementById('value-form').value = normalisePlace(e.sourceTarget.feature.properties.prov_name);
+            document.getElementById('search-form').submit();
+        });
     });
 }
 
@@ -84,7 +114,8 @@ Papa.parse('/dpc-covid19-ita-province.csv', {
         results.data.forEach(function(item) {
             var parsedDate = item.data.substr(0, 10);
             if (typeof history[parsedDate] === 'undefined') history[parsedDate] = {};
-            history[parsedDate][item.denominazione_provincia] = parseInt(item.totale_casi, 10);
+	    province = normalisePlace(item.denominazione_provincia);
+            history[parsedDate][province] = parseInt(item.totale_casi, 10);
         });
         var dataProvince = history[today] || history[yesterday];
         var lastUpdate = history[today] ? today : yesterday;
