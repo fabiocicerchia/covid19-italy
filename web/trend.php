e8ababbf69a13049eef14d930f0ac32b7726483b
Fabio Cicerchia <info@fabiocicerchia.it>
2020-04-12 18:38:31 +0000

    handling the json format (wip)

--- web/trend.php
+++ web/trend.php
@@ -1,9 +1,10 @@
 <?php
 
-$type  = preg_replace('/[^a-z]/i', '', $_GET['type']); // region or province
-$value = preg_replace('/[^a-z .\']/i', '', $_GET['value']); // region or province
+$type   = preg_replace('/[^a-z]/i', '', $_GET['type']); // region or province
+$value  = preg_replace('/[^a-z .\']/i', '', $_GET['value']); // region or province
+$format = $_GET['format'] === 'json' ? 'json' : 'text';
 
-$command = 'cd '.__DIR__.'/../ && ./bin/covid19-trend ';
+$command = 'cd '.__DIR__.'/../ && ./bin/covid19-trend --format ' . $format;
 if ($type === 'region') {
     $command .= '--region "' . $value . '"';
 } elseif ($type === 'province') {
